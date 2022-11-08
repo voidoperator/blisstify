c6486480abd52aec4217ed8b2c3f686a6b2b4e01
vz <julionunez@me.com>
2022-11-07 20:19:22 -0500

    grunt: basic file restructuring & components mapped

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+// import { SearchBar } from '../SearchBar';
+// import { SearchResults } from '../SearchResults';
+// import { Playlist } from '../Playlist';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <SearchBar /> */}
+        <div className="App-Playlist">
+          {/* <SearchResults /> */}
+          {/* <Playlist /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
